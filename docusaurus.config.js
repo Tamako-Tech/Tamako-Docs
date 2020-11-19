fbd9cc0c9e3b582836ac6f9740376b638f2a34b1
BearTS <bear@tamakobot.tk>
2020-11-19 19:36:23 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,16 +5,16 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/logo.png',
-  organizationName: 'Bear', // Usually your GitHub org/user name.
-  projectName: 'Tamako', // Usually your repo name.
+  organizationName: 'BearTS', // Usually your GitHub org/user name.
+  projectName: 'Tamako-Docs', 
   themeConfig: {
    announcementBar: {
-    id: 'support_us', // Any value that will identify this message.
+    id: 'support_us',
     content:
        'If you like Tamako, Vote her on <a target="_blank" href="https://top.gg/bot/721100913611112470">top.gg</a> | Invite  my Onnechan <a href="https://discord.com/oauth2/authorize?client_id=702074452317307061&scope=bot&permissions=1043721303">Mai</a>',
-     backgroundColor: '#fafbfc', // Defaults to `#fff`.
-      textColor: '#091E42', // Defaults to `#000`.
-      isCloseable: true, // Defaults to `true`.
+     backgroundColor: '#fafbfc', 
+      textColor: '#091E42',
+      isCloseable: true, 
   },
     navbar: {
        hideOnScroll: true,
